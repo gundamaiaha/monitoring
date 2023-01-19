4954c04a420d020bc276e352ece0b290a6b0d64c
Gundamaiah Annamaina <a.gundamaiah@gmail.com>
2023-01-19 20:48:18 +0530

    spring cache monitoring demo added

---
+++ spring-boot-micrometer/src/main/java/com/example/micrometer/api/SpringBootMicrometerApplication.java
@@ -0,0 +1,13 @@
+package com.example.micrometer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicrometerApplication.class, args);
+	}
+
+}
